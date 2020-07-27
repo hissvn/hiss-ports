1b8d3cc21818c7f1e9fa72ec6c257ac51038b408
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-07-27 13:55:25 -0600

    hissjs skeleton

---
+++ js/src/hissjs/Main.hx
@@ -0,0 +1,10 @@
+package hissjs;
+
+import hiss.CCInterp;
+
+class Main {	
+    static function main() {
+        var interp = new CCInterp();
+        interp.load("src/hissjs/main.hiss");
+    }
+}
