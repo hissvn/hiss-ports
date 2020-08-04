97c6933ff42cf39538de104e22059866834b9105
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-08-03 18:05:36 -0600

    hissjs workaround for nodejs lack of getChar()

--- scripts/hissjs.bat
+++ scripts/hissjs.bat
@@ -1,2 +1,2 @@
-cd %HOME%\hiss-ports\js && haxe build.hxml
-node %HOME%\hiss-ports\js\index.js %*
+@echo off
+cd %HOME%\hiss-ports\js && haxe build.hxml && node %HOME%\hiss-ports\js\index.js --no-console-reader %*
